a5a6e57900c79254ac7a64d347bee94689ac99f7
박성연 <tjddsu3171@naver.com>
2026-04-16 12:09:56 +0900

    04_16

--- 03_django_model/articles/models.py
+++ 03_django_model/articles/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Article(models.Model):
-    title = models.CharField(max_length=10)
-    content = models.TextField()
+    title = models.CharField(max_length=10) 
+    content = models.TextField() #길이 제한이 없음 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
